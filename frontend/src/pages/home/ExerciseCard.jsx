addddb1db0816a4730264d635658841e37c970e2
Parmod Kumar <parmod1feb2002@gmail.com>
2025-03-10 21:21:25 +0545

    Update ExerciseCard.jsx

--- frontend/src/pages/home/ExerciseCard.jsx
+++ frontend/src/pages/home/ExerciseCard.jsx
@@ -27,7 +27,8 @@ function ExerciseCard(){
         }
     }
 
-    
+    if(!isExercise) return <div  className='text-center p-20 h-full text-2xl font-bold'><h1>Loading...</h1></div>
+
     return(
         <section className='my-4 h-full mx-auto grid gap-3 w-[90%] sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 place-content-center'>
         
