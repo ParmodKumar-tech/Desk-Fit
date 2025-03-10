708febbe22a9820fb266e5a4341fb4fb4e920a0d
Parmod Kumar <parmod1feb2002@gmail.com>
2025-03-10 21:03:09 +0545

    Update EndPoint.js

--- frontend/src/utils/EndPoint.js
+++ frontend/src/utils/EndPoint.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT="http://localhost:4000/api/v1/user";
-export const RESOURCE_API_END_POINT="http://localhost:4000/api/v1";
+export const USER_API_END_POINT="https://desk-fit-backend.vercel.app/";
+export const RESOURCE_API_END_POINT="https://desk-fit-backend.vercel.app/";
