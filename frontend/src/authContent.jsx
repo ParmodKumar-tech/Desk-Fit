6b013668892068a76ce66cde909c2f982ff81d98
Parmod Kumar <parmod1feb2002@gmail.com>
2025-06-16 08:15:09 +0530

    Update authContent.jsx

--- frontend/src/authContent.jsx
+++ frontend/src/authContent.jsx
@@ -10,34 +10,17 @@ export const useAuth=()=>{
 
 export const AuthProvider=({children})=>{
     const [currentUserId,setCurrentUserId]=useState(null);
-    const [currentUserName,setCurrentUserName]=useState(null);
     const [currentUserEmail,setCurrentUserEmail]=useState(null);
 
     useEffect(()=>{
        getCurrUser();
     },[])
     
-    const getCurrUser=async()=>{
-        try{
-            const res=await axios.get(`${USER_API_END_POINT}/curr-user`,{withCredentials:true});
-            if(res.data.success){
-                setCurrentUserEmail(res.data.email);
-                setCurrentUserId(res.data.id);
-                setCurrentUserName(res.data.username);
-            }
-        }
-        catch(err){
-            toast.error(err.response.data.message);
-        }
-       
-        
-    }
-
+    
     const value={
         currentUserId,
         setCurrentUserId,
 
-        currentUserName,
         currentUserEmail,
         setCurrentUserEmail
     }
