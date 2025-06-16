5a96dacfe435248a486da005ced8cd4f6147237f
Parmod Kumar <parmod1feb2002@gmail.com>
2025-06-16 08:24:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT=process.env.PORT || 4000;
 
 connectDB();
 
-app.use(cors());
+app.use(cors({origin:"https://desk-fit-backend.vercel.app", credentials:true}));
 app.use(express.json());
 
 app.use("/api/v1/",ExerciseRoute);
