41634e1d7c20cda4a197e881b0683784eb7c9428
Parmod Kumar <parmod1feb2002@gmail.com>
2026-03-29 13:38:46 +0530

    Update API endpoint to use environment variable

--- frontend/src/api/api.exercises.js
+++ frontend/src/api/api.exercises.js
@@ -1,6 +1,6 @@
 
 import axios from "axios";
-export const RESOURCE_API_END_POINT="https://desk-fit-7grh-backend.vercel.app/api/v1";
+export const RESOURCE_API_END_POINT=import.meta.env.VITE_RESOURCE_API;
 
 export const getAllExecrise= async()=>{
      try{
